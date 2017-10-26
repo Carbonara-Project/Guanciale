2550fd4c30b96d68be2d4ca2237acbdbd387db5d
Andrea Fioraldi <andreafioraldi@gmail.com>
2017-10-26 21:48:16 +0200

    work on AbstractProcedure class started

--- carbonara_cli/matching.py
+++ carbonara_cli/matching.py
@@ -9,28 +9,147 @@ import pyvex
 import archinfo
 import hashlib
 
-vex_arch_map = {
-    "AMD64": archinfo.ArchAMD64,
-    
+r2_arch_map = {
+    ("arm", 64): archinfo.ArchAArch64,
+    ("arm", 32): archinfo.ArchARM,
+    ("x86", 64): archinfo.ArchAMD64,
+    ("avr", 16): archinfo.ArchAVR,
+    ("mips", 32): archinfo.ArchMIPS32,
+    ("mips", 64): archinfo.ArchMIPS64,
+    ("ppc", 32): archinfo.ArchPPC32,
+    ("ppc", 64): archinfo.ArchPPC64,
+    ("x86", 32): archinfo.ArchX86
 }
+
+''' HOW TO GET ARCH IN IDAPYTHON?
+import idaapi
+
+info = idaapi.get_inf_structure()
+
+if info.is_64bit():
+    bits = 64
+elif info.is_32bit():
+    bits = 32
+else:
+    bits = 16
+
+endian = "little"
+if info.mf:
+    endian = "big"
+'''
+
+ida_arch_map = {
+    ("arm", 64): archinfo.ArchAArch64,
+    ("armb", 64): archinfo.ArchAArch64,
+    ("arm", 32): archinfo.ArchARM,
+    ("armb", 32): archinfo.ArchARM,
+    ("metapc", 64): archinfo.ArchAMD64,
+    ("avr", 16): archinfo.ArchAVR, #??? check if it is true in ida
+    ("mips", 32): archinfo.ArchMIPS32,
+    ("mipsb", 32): archinfo.ArchMIPS32,
+    ("mips64", 64): archinfo.ArchMIPS64,
+    ("mips64b", 64): archinfo.ArchMIPS64,
+    ("ppc", 32): archinfo.ArchPPC32,
+    ("ppcb", 32): archinfo.ArchPPC32,
+    ("ppc64", 64): archinfo.ArchPPC64,
+    ("ppc64b", 64): archinfo.ArchPPC64,
+    ("metapc", 32): archinfo.ArchX86
+    #TODO add more ida processors in the map
+}
+
+def archFromR2(arch, bits, endian):
+    a = r2_arch_map[(arch, bits)]
+    if endian != "little":
+        return a(archinfo.Endness.BE)
+    return a(archinfo.Endness.LE)
+
+def archFromIda(processor, bits, endian):
+    a = ida_arch_map[(arch, bits)]
+    if endian != "little":
+        return a(archinfo.Endness.BE)
+    return a(archinfo.Endness.LE)
+
+
+
+class CallInsn(object):
+    def __init__(self, offset, size, fcn_addr, fcn_name, is_api=False):
+        self.offset = offset
+        self.size = size
+        self.fcn_addr = fcn_addr
+        self.fcn_name = fcn_name
+        self.is_api = is_api
+
+class JumpInsn(object):
+    def __init__(self, offset, size, addr, jumpout=False):
+        self.offset = offset
+        self.size = size
+        self.addr = addr
+        self.jumpout = jumpout
 
 
 class AbstractProcedure(object):
-    def __init__(code, offset, size):
+    def __init__(code, offset, calls_insns, jumps_insns, arch):
         self.code = code
         self.offset = offset
-        self.size = size
+        self.endaddr = offset + len(code)
+        self.calls_insns = calls_insns
+        self.jumps_insns = jumps_insns
+        self.arch = arch
     
     def lift_helper(self, irsb):
+        stmts = irsb.statements
         
+        for i in xrange(len(stmts)):
+            #TODO PutI GetI Exit
+            
+            # registers abstraction
+            if isinstance(stmts[i], pyvex.stmt.Put):
+                regs[stmts[i].offset] = regs.get(stmts[i].offset, len(regs))
+                stmts[i].offset = regs[stmts[i].offset]
+                
+                if i+1 < len(stmts) and isinstance(stmts[i+1], pyvex.stmt.IMark) and len(stmts[i].constants) == 1: #problably program counter? self.arch["pc"]
+                    ips.append(stmts[i].constants[0].value)
+                else:
+                    # constants abstraction
+                    for c in stmts[i].constants:
+                        consts[c.value] = consts.get(c.value, len(consts))
+                        c.value = consts[c.value]
+            else:
+                # constants abstraction
+                for c in stmts[i].constants:
+                    consts[c.value] = consts.get(c.value, len(consts))
+                    c.value = consts[c.value]
+            for expr in stmts[i].expressions:
+                if isinstance(expr, pyvex.expr.Get):
+                    regs[expr.offset] = regs.get(expr.offset, len(regs))
+                    expr.offset = regs[expr.offset]
+            
+        for i in xrange(len(stmts)):
+            if isinstance(stmts[i], pyvex.stmt.IMark) or isinstance(stmts[i], pyvex.stmt.AbiHint):
+                continue
+            
+            if isinstance(stmts[i], pyvex.stmt.Put) and  i+1 < len(stmts) and isinstance(stmts[i+1], pyvex.stmt.IMark) and len(stmts[i].constants) == 1:
+                stmts[i].constants[0].value = addrs[stmts[i].constants[0].value]
+            
+        vex_code += stmts[i].__str__() + "\n"
+        
+        for tgt in irsb.constant_jump_targets:
+            
+            if tgt < self.offset and tgt >= self.endaddr:
+                continue
+            if tgt in self.bb: # skip loops
+                continue
+            nirsb = pyvex.IRSB(self.code[tgt - self.offset:], tgt, self.arch)
+            self.lift_helper(nirsb)
     
     def lift(self):
         self.consts = {}
         self.regs = {}
         self.ips = []
-        self.vexcode = ""
+        self.bb = [self.code]
+        self.vex_code = ""
         
-        irsb = pyvex.IRSB(self.code, offset, archinfo.ArchAMD64())
+        irsb = pyvex.IRSB(self.code, self.offset, self.arch)
         self.lift_helper(irsb)
 
 
