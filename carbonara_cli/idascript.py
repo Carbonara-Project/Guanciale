5f2d18ab2142e22ac38c17f1311740b156061054
rop2bash <rop2bash@gmail.com>
2017-11-01 12:39:06 +0100

    apicalls -> flow_insns

--- carbonara_cli/idascript.py
+++ carbonara_cli/idascript.py
@@ -15,7 +15,7 @@ name | OK
 raw ! OK
 asm | OK
 offset |OK
-apicalls | TODO
+flow_insns | almost done
 callconv | TODO
 '''
 
@@ -25,56 +25,72 @@ idaapi.autoWait()
 #DEBUG
 f = open('debug.txt', 'w')
 
-f.write('o_mem: '+str(o_mem)+'\n')
-f.write('o_far: '+str(o_far)+'\n')
-f.write('o_near: '+str(o_near)+'\n')
-
-
 
 #iterate trhough functions
 for func in idautils.Functions():
 
 	#if func from library skip
 	flags = idc.GetFunctionFlags(func)
-	if flags & FUNC_LIB:
-		continue
-	if flags & FUNC_THUNK:
-		continue
-	if flags & FUNC_HIDDEN:
+	if flags & FUNC_LIB or flags & FUNC_THUNK or flags & FUNC_HIDDEN:
 		continue
 
 	#get procedure name
 	name = idc.GetFunctionName(func)
 	
-	#get assembly
 	start = idc.GetFunctionAttr(func, FUNCATTR_START)
 	end = idc.GetFunctionAttr(func, FUNCATTR_END)
 	cur_addr = start
 
 	apicalls = []
+	call_insns = []
+	jmp_insns = []
 	asm = ''
 
 	while cur_addr <= end:
+
+		next_instr = idc.NextHead(cur_addr, end)
+
+		#get size instr
+		if next_instr > end:
+			size = end - cur_addr
+		else:
+			size = next_instr - cur_addr
+
+		#get assembly and comments
 		asm += hex(cur_addr) + ' ' + idc.GetDisasm(cur_addr) +'\n'
 
 		#check if api call/jump
 		mnem = idc.GetMnem(cur_addr)
-		if mnem == 'call' or mnem == 'jmp':
+		if mnem == 'call':
 			op = idc.GetOpnd(cur_addr, 0)
 			op_type = idc.GetOpType(cur_addr, 0)
 			#addr = idc.LocByname(op)
 			#check = idc.GetFunctionFlags(addr)
-			apicalls.append((op, op_type))
+			
+			if op[0] == '_': #temp test if api (not 100% reliable)
+				func_name = op[1:]
+			else:
+				func_name = op
+			
+			target = 0 #TODO
+			call_insns.append((hex(cur_addr)[:-1], size, func_name, target))
 
+		elif mnem == 'jmp':
+			op = idc.GetOpnd(cur_addr, 0)
+			op_type = idc.GetOpType(cur_addr, 0)
 
-		cur_addr = idc.NextHead(cur_addr, end)
+			target = 0 #TODO
+			jumpout = 0 #need target to know if jumpout
+			jmp_insns.append((hex(cur_addr)[:-1], size, target, jumpout))
+
+		cur_addr = next_instr
 
 
 	#get raw data
 	raw_data = idc.GetManyBytes(start, end - start)
 
 	#DEBUG
-	f.write(hex(start)[:-1] +' '+ name+ ';flags: '+hex(flags)[:-1]+'\napicalls: '+ str(apicalls)+ '\n')
+	f.write(hex(start)[:-1] +' '+ name+ ';flags: '+hex(flags)[:-1]+'\ncall_insns: '+ str(call_insns)+'; jmp_insns: '+str(jmp_insns)+'\n')
 	#f.write(asm)
 	#f.write('\n')
 
