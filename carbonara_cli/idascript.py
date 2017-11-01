78235fdc46241d26c2d2fcca3f88535d86e52436
rop2bash <rop2bash@gmail.com>
2017-11-01 10:00:18 +0100

    working on apicalls

--- carbonara_cli/idascript.py
+++ carbonara_cli/idascript.py
@@ -25,12 +25,22 @@ idaapi.autoWait()
 #DEBUG
 f = open('debug.txt', 'w')
 
+f.write('o_mem: '+str(o_mem)+'\n')
+f.write('o_far: '+str(o_far)+'\n')
+f.write('o_near: '+str(o_near)+'\n')
+
+
+
 #iterate trhough functions
 for func in idautils.Functions():
 
 	#if func from library skip
 	flags = idc.GetFunctionFlags(func)
 	if flags & FUNC_LIB:
+		continue
+	if flags & FUNC_THUNK:
+		continue
+	if flags & FUNC_HIDDEN:
 		continue
 
 	#get procedure name
@@ -41,7 +51,7 @@ for func in idautils.Functions():
 	end = idc.GetFunctionAttr(func, FUNCATTR_END)
 	cur_addr = start
 
-	apicalls = 0
+	apicalls = []
 	asm = ''
 
 	while cur_addr <= end:
@@ -50,20 +60,23 @@ for func in idautils.Functions():
 		#check if api call/jump
 		mnem = idc.GetMnem(cur_addr)
 		if mnem == 'call' or mnem == 'jmp':
-			addr = idc.GetOpnd(cur_addr, 0)
-			check = idc.GetFunctionFlags(func)
-			if check & FUNC_LIB:
-				apicalls += 1
+			op = idc.GetOpnd(cur_addr, 0)
+			op_type = idc.GetOpType(cur_addr, 0)
+			#addr = idc.LocByname(op)
+			#check = idc.GetFunctionFlags(addr)
+			apicalls.append((op, op_type))
+
 
 		cur_addr = idc.NextHead(cur_addr, end)
+
 
 	#get raw data
 	raw_data = idc.GetManyBytes(start, end - start)
 
 	#DEBUG
-	f.write(hex(start) +' '+ name+'\n')
-	f.write(asm)
-	f.write('\n')
+	f.write(hex(start)[:-1] +' '+ name+ ';flags: '+hex(flags)[:-1]+'\napicalls: '+ str(apicalls)+ '\n')
+	#f.write(asm)
+	#f.write('\n')
 
 f.close()
 
