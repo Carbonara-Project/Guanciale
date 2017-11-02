ce7a9ec7b2fa1a96e1426fd1454207412cbce35d
rop2bash <rop2bash@gmail.com>
2017-11-02 22:44:38 +0100

    idascript: got program class (temporary mapping into idascript.py)

--- carbonara_cli/idascript.py
+++ carbonara_cli/idascript.py
@@ -14,7 +14,7 @@ import base64
 
 '''
 info: (Where is even located all this info in IDA?) (edit: God bless tmr232 from StackOverflow)
-    program_class | TODO
+    program_class | OK
     arch | OK
     bits | OK
     endian | OK
@@ -24,12 +24,41 @@ procedures:
     asm | OK
     offset |OK
     flow_insns | OK
-    callconv | TODO => PROB NOT POSSIBLE (i love IDA)
+    callconv | TODO => PROB NOT POSSIBLE (i love IDA\n ps: please save me)
 ops | OK
 imports | TODO
 exports | TODO
 libs | TODO
 '''
+
+ida_class_map = {
+    0:  'EXE_old',     # MS DOS EXE File
+    1:  'COM_old',     # MS DOS COM File
+    2:  'BIN',         # Binary File
+    3:  'DRV',         # MS DOS Driver
+    4:  'WIN',         # New Executable (NE)
+    5:  'HEX',         # Intel Hex Object File
+    6:  'MEX',         # MOS Technology Hex Object File
+    7:  'LX',          # Linear Executable (LX)
+    8:  'LE',          # Linear Executable (LE)
+    9:  'NLM',         # Netware Loadable Module (NLM)
+    10: 'COFF',        # Common Object File Format (COFF)
+    11: 'PE',          # Portable Executable (PE)
+    12: 'OMF',         # Object Module Format
+    13: 'SREC',        # R-records
+    14: 'ZIP',         # ZIP file (this file is never loaded to IDA database)
+    15: 'OMFLIB',      # Library of OMF Modules
+    16: 'AR',          # ar library
+    17: 'LOADER',      # file is loaded using LOADER DLL
+    18: 'ELF',         # Executable and Linkable Format (ELF)
+    19: 'W32RUN',      # Watcom DOS32 Extender (W32RUN)
+    20: 'AOUT',        # Linux a.out (AOUT)
+    21: 'PRC',         # PalmPilot program file
+    22: 'EXE',         # MS DOS EXE File
+    23: 'COM',         # MS DOS COM File
+    24: 'AIXAR',       # AIX ar library
+    25: 'MACHO'        # Max OS X
+}
 
 #wait for IDA analysys to complete
 idaapi.autoWait()
@@ -71,6 +100,9 @@ else:
 data['info']['endian'] = "little"
 if info.is_be():
     data['info']['endian'] = "big"
+
+#get program_class
+data['info']['program_class'] = class_map[info.filetype]
 
 #iterate through functions
 for func in idautils.Functions():
@@ -149,7 +181,7 @@ for func in idautils.Functions():
     }
     data['procedures'].append(proc_data)
 
-f.write(data['info']['arch']+" "+str(data['info']['bits'])+" "+data['info']['endian']+"\n"+str(data['ops'])+'\n')
+f.write(data['info']['program_class']+ " "+data['info']['arch']+" "+str(data['info']['bits'])+" "+data['info']['endian']+"\n"+str(data['ops'])+'\n')
 json.dump(data, dump)
 
 f.close()
